fc79943ea5277e9f28101fa36fa4c331c31ca86a
Michael L Perry <michael@qedcode.com>
2024-12-25 13:23:09 -0600

    Use promise version of fs

--- loadPolicies.ts
+++ loadPolicies.ts
@@ -1,4 +1,4 @@
-import { Dirent, readFile, readdir } from "fs";
+import { readFile, readdir } from "fs/promises";
 import { RuleSet } from "jinaga";
 import { join } from "path";
 
@@ -34,15 +34,7 @@ async function findPolicyFiles(dir: string): Promise<{ policyFiles: string[], ha
     const policyFiles: string[] = [];
     let hasMarkerFile = false;
 
-    const entries = await new Promise<Dirent[]>((resolve, reject) => {
-        readdir(dir, { withFileTypes: true }, (err, files) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(files);
-            }
-        });
-    });
+    const entries = await readdir(dir, { withFileTypes: true });
 
     for (const entry of entries) {
         const fullPath = join(dir, entry.name);
@@ -63,16 +55,7 @@ async function findPolicyFiles(dir: string): Promise<{ policyFiles: string[], ha
 }
 
 async function loadRuleSetFromFile(path: string): Promise<RuleSet> {
-    const description = await new Promise<string>((resolve, reject) => {
-        readFile(path, 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-
+    const description = await readFile(path, 'utf8');
     const ruleSet = RuleSet.loadFromDescription(description);
     return ruleSet;
 }
