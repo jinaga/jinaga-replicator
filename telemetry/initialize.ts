32975ccfaa66608a949801bb71b4128e4700e6eb
Michael L Perry <michael@qedcode.com>
2025-02-22 22:19:02 -0600

    Add support for OpenTelemetry metrics by integrating sdk-metrics and updating initialization

--- telemetry/initialize.ts
+++ telemetry/initialize.ts
@@ -1,6 +1,6 @@
-const { trace }  = require("@opentelemetry/api");
-const { BasicTracerProvider, ConsoleSpanExporter, SimpleSpanProcessor }  = require("@opentelemetry/sdk-trace-base");
-
+const { trace, metrics, logs }  = require("@opentelemetry/api");
+const { BasicTracerProvider, SimpleSpanProcessor }  = require("@opentelemetry/sdk-trace-base");
+const { MeterProvider } = require('@opentelemetry/sdk-metrics');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { NodeSDK } = require('@opentelemetry/sdk-node');
@@ -13,12 +13,20 @@ if (!OTEL_EXPORTER_OTLP_ENDPOINT) {
   console.log('OTEL_EXPORTER_OTLP_ENDPOINT is not set. Tracing will not be enabled.');
 }
 else {
-    const provider = new BasicTracerProvider();
-    provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-    trace.setGlobalTracerProvider(provider);
+    const tracerProvider = new BasicTracerProvider();
     const traceExporter = new OTLPTraceExporter({
         url: OTEL_EXPORTER_OTLP_ENDPOINT,
     });
+    tracerProvider.addSpanProcessor(new SimpleSpanProcessor(traceExporter));
+    trace.setGlobalTracerProvider(tracerProvider);
+
+    const metricsExporter = new OTLPTraceExporter({
+        url: OTEL_EXPORTER_OTLP_ENDPOINT,
+    });
+    const metricsProvider = new MeterProvider({
+        exporter: metricsExporter,
+    });
+    metrics.setGlobalMeterProvider(metricsProvider);
     sdk = new NodeSDK({
         traceExporter,
         instrumentations: [getNodeAutoInstrumentations()],
